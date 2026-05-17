77f445bdd5e89d238db79e78757c5be199ad4c1f
HookHatai <itmailmiri@gmail.com>
2026-05-17 18:49:03 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // Замени 'futuristic-website-design' на точное название твоего репозитория на GitHub
+  basePath: '/RuaSide',
   typescript: {
     ignoreBuildErrors: true,
   },
